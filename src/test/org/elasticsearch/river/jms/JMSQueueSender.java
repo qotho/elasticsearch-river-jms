7e2a06d50c94f99ded0aec0406bdd79c0c641d34
iooab23 <temp@fake.com>
2013-04-29 10:01:47 -0300

    Create JMSQueueSender.java

---
+++ src/test/org/elasticsearch/river/jms/JMSQueueSender.java
@@ -0,0 +1,57 @@
+package org.elasticsearch.river.jms;
+
+import java.io.*;
+import java.util.Hashtable;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class JMSQueueSender {
+  public final static String JNDI_FACTORY = "weblogic.jndi.WLInitialContextFactory";
+	public final static String JMS_FACTORY = "jms/ElasticSearchConnFactory";
+
+	private QueueConnectionFactory qconFactory;
+	private QueueConnection qcon;
+	private QueueSession qsession;
+	private QueueSender qsender;
+	private Queue queue;
+	private TextMessage msg;
+
+	public JMSQueueSender(String providerUrl, String queueName) throws NamingException,	JMSException {
+		InitialContext ctx = getInitialContext(providerUrl);
+		qconFactory = (QueueConnectionFactory) ctx.lookup(JMS_FACTORY);
+		qcon = qconFactory.createQueueConnection();
+		qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+		//queue = qsession.createQueue(QUEUE);
+		queue = (Queue) ctx.lookup(queueName);
+		qsender = qsession.createSender(queue);
+		msg = qsession.createTextMessage();
+		qcon.start();
+	}
+
+	public void send(String message) throws JMSException {
+		msg.setText(message);
+		qsender.send(msg);
+	}
+
+	public void close() throws JMSException {
+		qsender.close();
+		qsession.close();
+		qcon.close();
+	}
+
+	private static InitialContext getInitialContext(String url)	throws NamingException {
+		Hashtable env = new Hashtable();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, JNDI_FACTORY);
+		env.put(Context.PROVIDER_URL, url);
+		return new InitialContext(env);
+	}
+}
